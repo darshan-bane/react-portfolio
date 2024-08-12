5e0f668796718387c14d0faa215d0302f7390c8c
darshan-bane <darshanbane253@gmail.com>
2024-08-12 15:51:04 +0530

    experience

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,4 +1,64 @@
+import { FaBookReader } from "react-icons/fa";
+import TimelineItem from "./TimelineItem";
+import { MdWork } from "react-icons/md";
+
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <section>
+      <header>
+        <h2 className="h2 article-title">Resume</h2>
+      </header>
+      <div className="timeline">
+        <div className="title-wrapper">
+          <div className="icon-box">
+            <FaBookReader />
+          </div>
+          <h3 className="h3">Education</h3>
+        </div>
+        <ol className="timeline-list">
+          <TimelineItem
+            title="BSC.I.T from Kirti M. Dongursee College"
+            date="2020-2023"
+            description="lorem ispem lorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispem"
+          />
+          <TimelineItem
+            title="HSC from Bhavans Hazarimal Somani College"
+            date="2019-2020"
+            description="lorem ispem lorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispem"
+          />
+          <TimelineItem
+            title="SSC from Antonio De Souza High School"
+            date="2017-2018"
+            description="lorem ispem lorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispem"
+          />
+        </ol>
+      </div>
+      <div className="timeline">
+        <div className="title-wrapper">
+          <div className="icon-box">
+            <MdWork />
+          </div>
+          <h3 className="h3">Experience</h3>
+        </div>
+        <ol className="timeline-list">
+          <TimelineItem
+            title="BSC.I.T from Kirti M. Dongursee College"
+            date="2020-2023"
+            description="lorem ispem lorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispem"
+          />
+          <TimelineItem
+            title="HSC from Bhavans Hazarimal Somani College"
+            date="2019-2020"
+            description="lorem ispem lorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispem"
+          />
+          <TimelineItem
+            title="SSC from Antonio De Souza High School"
+            date="2017-2018"
+            description="lorem ispem lorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispemlorem ispem"
+          />
+        </ol>
+      </div>
+    </section>
+  );
 };
 export default Resume;
